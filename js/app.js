9eb2caf653d2a0da0a032e0754727c911a6670a8
llihwerd <100llihwerd@gmail.com>
2023-12-26 23:15:41 -0500

    set up initial functions and start button

---
+++ js/app.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+  let startButton = document.querySelector('#start-button')
+  let input = document.querySelector('#name')
+  startButton.addEventListener('click', function() {
+  console.log(input.value)
+  })
+})
+
+
+function play() {
+
+}
+
+function advance() {
+
+}
+
